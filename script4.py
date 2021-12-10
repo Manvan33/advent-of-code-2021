00e058357c1edb9977db5c87fd9d3b3b64b1f12c
Manvan33 <rikostik@gmail.com>
2021-12-10 17:07:21 +0100

    start of day4

---
+++ script4.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def parse_input(input: list[str]) -> tuple[list[int], list[list[int]]]:   
+    order = [int(number) for number in input.pop(0).split(",")]
+    boards = []
+    for line in input()
+    return order, 0
+
+def check_win(board: list[list[int]]):
+    return 1
+
+def main():
+    with open("input4") as input_file:
+        order, boards = parse_input(input_file.readlines())
+
+
+if __name__ == "__main__":
+    main()
